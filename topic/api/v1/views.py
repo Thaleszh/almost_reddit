f8356e5f22a187458b7cb1e79efb88819bf26bb5
Thaleszh <thaleszh@gmail.com>
2020-10-30 17:15:24 -0300

    Fixed Permissionsv2

--- topic/api/v1/views.py
+++ topic/api/v1/views.py
@@ -1,5 +1,4 @@
 from rest_framework.generics import get_object_or_404
-from rest_framework.permissions import BasePermission, SAFE_METHODS
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
