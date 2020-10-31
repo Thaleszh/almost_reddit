c8a2b3c1f29e151fcca04a2b8f8aab24683d9767
Thaleszh <thaleszh@gmail.com>
2020-10-31 14:27:48 -0300

    Cleanup

--- comment/api/v1/views.py
+++ comment/api/v1/views.py
@@ -1,10 +1,8 @@
-from rest_framework.generics import get_object_or_404
 from rest_framework.viewsets import ModelViewSet
 
 from accounts.api.v1.permissions import IsOwnerOrReadOnly
 from comment.api.v1.serializer import CommentSerializer
 from comment.models import Comment
-from post.models import Post
 
 
 class CommentViewSet(ModelViewSet):
@@ -15,7 +13,3 @@ class CommentViewSet(ModelViewSet):
     def filter_queryset(self, queryset):
         current_post_pk = self.kwargs.get('post_pk')
         return queryset.filter(post_id=current_post_pk)
-
-    def perform_create(self, serializer):
-        post = get_object_or_404(Post, id=self.kwargs.get('post_pk'))
-        serializer.save(post=post)
--- post/api/v1/views.py
+++ post/api/v1/views.py
@@ -1,10 +1,8 @@
-from rest_framework.generics import get_object_or_404
 from rest_framework.viewsets import ModelViewSet
 
 from accounts.api.v1.permissions import IsOwnerOrReadOnly
 from post.api.v1.serializer import PostSerializer
 from post.models import Post
-from topic.models import Topic
 
 
 class PostViewSet(ModelViewSet):
@@ -16,6 +14,3 @@ class PostViewSet(ModelViewSet):
         topic_url_name = self.kwargs.get('topic_url_name')
         return queryset.filter(topic__url_name=topic_url_name)
 
-    def perform_create(self, serializer):
-        topic = get_object_or_404(Topic, url_name=self.kwargs.get('topic_url_name'))
-        serializer.save(topic=topic)
